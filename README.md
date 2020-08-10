f602da0ada7886f4aec2c07c4be8decaec9e8414
Alex Zhu <ayjzhu@gmail.com>
2020-08-10 03:07:52 -0700

    completed search by names and id along with download

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# ncmBot
-A discord music bot plays via Netease music
+# NetEase Cloud Music Bot
+A Discord music bot that play music on demand via NetEase Cloud Music
+
+## Key Features
+- Listen and share music toegether in a group
+- Login and connect with personal NetEase Music account
+    - Fetch user information
+    - Fetch user's playlists and import to player queue
+    - Add or modify personal playlists
+- View popular comments of a song
+- Toggle lyric for current playing song
+- Interative voting with `Like` or `dislike` a song:
+  - Liking a song will be added to user's personal favorite playlist within the current Discord server
+  - When a playing song gets a certain number of downvotes, it will be automatically __skipped__!
+- Various ways of searching music:
+    - By song names
+    - By song id or url
+    - By playlist name or url
+- Provide download to songs
+
+## Development Roadmap
+- Basic playback functionalities of a music player
+    - [ ] Load or import playlist to queue
+    - [ ] Play songs in the queue
+    - [x] Song downloading
+    - [ ] Stop player and properly clear queue
+- Admin
+    - [x] Load or unload a category of commands while the bot is running
+- Search engine
+  - [x] Song/artist name
+  - [ ] Song url
+  - [ ] Playlist url
+  - [ ] Precise type searching
+  - [ ] Keywords recommendations
+- Unique features
+    - [ ] User login
+    - [ ] Display popular comments
+    - [ ] Display lyric
+    - [ ] Fetch user infomation
+    - [ ] Fetch user's playlists
+    - [ ] Interative voting of current song
+
+## API In-Use
+- Netease Cloud Music
+- Discord Py
+
+## Supported Source
+- Netease Music
+- QQ Music (upcoming)
+
+## Usages/Commands
+The following commands start with a `.` symbol.
+<br>*(ex. .play)*
+___
+
+### Playback
+|Command|Function|
+|---|---|
+|play|Play the music|
+|skip|Skip the current song|
+|pause|Pause the music|
+|stop|Stop the music|
+|...|...|
+
+### Cog*
+|Command|Function|
+|---|---|
+|load|Load the extension file|
+|unload|Load the extension file|
+|reload|reload the extension file|
--- neteaseMusic.py
+++ neteaseMusic.py
@@ -3,6 +3,7 @@ import requests
 import datetime
 from prettytable import PrettyTable
 from pprint import pprint
+from configparser import ConfigParser
 
 class NeteaseMusic():
     '''
@@ -10,7 +11,12 @@ class NeteaseMusic():
     '''
 
     def __init__(self):
-        self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+        self.config = ConfigParser()
+        self.config.read("config.ini", encoding="UTF-8")
+        self.headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+            'Cookie': self.config.get('cred', 'cookie')
+        }
         self.baseUrl = 'https://ncm-api.herokuapp.com/'
         self.musicData = {
             'query' : dict(
@@ -20,6 +26,7 @@ class NeteaseMusic():
             'info' : dict()
         }
 
+
     #1: 单曲, 10: 专辑, 100: 歌手, 1000: 歌单, 1002: 用户, 1004: MV, 1006: 歌词, 1009: 电台, 1014: 视频, 1018:综合
     def search(self, keywords:str, limit=10, offset=0, type=0):
         params = {
@@ -28,10 +35,12 @@ class NeteaseMusic():
             'offset' : offset,
             'type' : 1,
         }
-        resp = requests.get('{}search'.format(self.baseUrl),
-                                headers=self.headers,
-                                params=params
+        resp = requests.get(
+            '{}search'.format(self.baseUrl),
+            headers=self.headers,
+            params=params
         ).json()
+
         # obtain data for songs
         songs = resp['result']['songs']
 
@@ -49,34 +58,81 @@ class NeteaseMusic():
                 'album': {
                     'name': song['album']['name'],
                     'id': song['album']['id'],
-                    # 'albumPic': song['album']['picUrl'],
                     'publishTime': convert(song['album']['publishTime']/1000),                                   # convert ms to seconds
                     'picid' : 18651015743755216
                 },
                 'length': '%02d:%02d' %(divmod(song['duration']/1000, 60)),                                    # convert ms to seconds
                 'songid': song['id'],
-                # 'size': {size:'{:.1f} MB'.format(value/1_000_000) for size, value in sizes.items()},    # convert each bitrates to megabytes
                 'songurl': 'https://music.163.com/#/song?id=%s' % song['id']
             }
-        
-            results.append(result)
 
+            # the filename contains all of the artist names
+            names = ' & '.join([i['name'] for i in result['artist']])
+            filename = '%s - %s' % (names, result['title'])
+            result.update(filename=filename)                # save the audio file name
+
+            results.append(result)       # save each song info to a list
+        # save the list the song info to the dictionary
         self.musicData.update(info = results)
-        # pprint(self.musicData)
 
         return self.musicData
 
-    def getDownloadLink(self, id:int, bitrate = 999000):
+
+    def search_by_id(self, id:int, bitrate=999000):
         params = {
             'id' : id,
             'br' : bitrate
         }
-        resp = requests.get('{}song/url'.format(self.baseUrl),
-                                headers = self.headers,
-                                params = params
-        ).json()
 
-        pprint(resp)
+        try:
+            # get the current song info
+            resp = requests.get(
+                '{}song/url'.format(self.baseUrl),
+                headers = self.headers,
+                params = params,
+            ).json()
+
+            pprint(resp)
+
+            # validate status code
+            if resp['code'] != 200 or resp['data'][0]['code'] != 200:
+                raise ValueError('Invalid! Please double check your song ID.')
+        except Exception as e:
+            print(f'ERROR: {type(e).__name__} - {e}')
+        else:
+            song = resp['data'][0]
+            songData = {
+                'id' : id,
+                'url' : song['url'],
+                'bitrate' : int(song['br'] / 1000),
+                'size' : '%.1f MB' % (song['size']/1_000_000),   # convert each bitrates to megabytes
+                'type' : song['type'],
+                'quality' : song['level'],
+                'fee' : 'vip only' if song['fee'] == 1 else 'free'
+            }
+            return songData
+
+
+    def download(self, song:dict, bitrate=320000):
+
+        # get the detail info of the song
+        songInfo = self.search_by_id(song['songid'], bitrate=bitrate)
+        try:
+            # get the binary data from the download link
+            data = requests.get(songInfo['url'], headers = self.headers).content
+
+            # create a new directory if haven't done so
+            fileDir = 'songs/'
+            if not os.path.exists(fileDir):
+                os.mkdir(fileDir)
+
+            with open('%s%s.%s' % (fileDir, song['filename'], songInfo['type']), 'wb') as f:
+                f.write(data)
+                print("{}.{} {} download completed!".format(song['filename'], songInfo['type'], song['length']))
+            return song['filename']
+        except Exception as e:
+            print(f'ERROR: {type(e).__name__} - {e}')
+
 
     def display(self):
         '''
@@ -101,8 +157,26 @@ class NeteaseMusic():
 
         return table
 
+    def login(self):
+        resp = requests.get(
+            '{}login/status'.format(self.baseUrl),
+            headers = self.headers
+        )
+        print(resp.status_code)
+
 if __name__ == "__main__":
     nm = NeteaseMusic()
-    # result = nm.search('minute', limit = 10)
+
+    ## testing search
+    result = nm.search('minute', limit = 10)
     # nm.display()
-    nm.getDownloadLink(123)
+    song = result['info'][1]
+    pprint(song)
+
+    ## testing download 1450574147  21224431  318143 1308010773
+    # pprint(nm.search_by_id(2990399))
+    nm.download(song, bitrate=990000)
+
+    ## testing login functions
+    # nm.login()
+
