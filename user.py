2892d62fd46b1c4045ab3efdf7b8d200e2c35c97
Alex Zhu <ayjzhu@gmail.com>
2020-08-13 23:46:49 -0700

    added login and data fetching for User class

--- user.py
+++ user.py
@@ -13,89 +13,154 @@ class User():
         }
         self.baseUrl = self.config.get('cred', 'ncmApi')
 
+        self.isLogIn = False
+        self.uid = uid
         self.userData = {
-            'account' : dict(),
+            'account' : dict(),     # obtain through login
             'profile' : dict(),
             'playlists' : dict(
                 count = 0,
+                subscribers = 0
             )
         }
-        self.isLogIn = False
-        self.cookie = None
+
 
     def login(self, email:str, pwd:str):
-        params = {
-            'email' : email,
-            'password' : pwd
-        }
         try:
             # get the current song info
             resp = requests.get(
                 '{}login'.format(self.baseUrl),
                 headers = self.headers,
-                params = params
+                params = {
+                            'email' : email,
+                            'password' : pwd
+                        }
             ).json()
 
             # Login validation
             if resp['code'] != 200:
-                raise ValueError('Incorrect! Please verify your password and try again.')
+                raise ValueError('Incorrect! Please verify your username or password and try again.')
             else:
                 print("Successfully logged in!")
-                self.cookie = resp['cookie']
                 self.isLogIn = True
                 # Retrieve account infomation
                 self.userData['account'].update(
                     userid = resp['account']['id'],
                     salt = resp['account']['salt'],
-                    cookie = resp['cookie'],
-                    isVip = True if resp['account']['vipType'] > 0 else False,
-                    dateCreated = resp['account']['createTime']
+                    token = resp['token'],
+                    cookie = resp['cookie']
                 )
+                return self.userData
+        except Exception as e:
+            print(f'ERROR: {type(e).__name__} - {e}')
 
+
+    def get_user(self, uid:int):
+        try:
+            # get the current song info
+            resp = requests.get(
+                '{}user/detail'.format(self.baseUrl),
+                headers = self.headers,
+                params= {
+                    'uid' : uid
+                }
+            ).json()
+
+            if resp['code'] != 200:
+                raise ValueError('Invalid! Please double check your user ID.')
+            else:
                 # Retrieve personal profile infomation
                 profile_data = {
                     'userid' : resp['profile']['userId'],
                     'userName' : resp['profile']['nickname'],
                     'signature' : resp['profile']['signature'],
-                    'avatarPic' : {
-                        'id' : resp['profile']['avatarImgId'],
-                        'url' : resp['profile']['avatarUrl']
-                    },
-                    'backgroundPic' : {
-                        'id' : resp['profile']['backgroundImgId'],
-                        'url' : resp['profile']['backgroundUrl']
+                    'level' : resp['level'],
+                    'listenSongs' : resp['listenSongs'],
+                    'balance' : resp['userPoint']['balance'],
+                    'updateTime' : resp['userPoint']['updateTime'],
+                    'picture': {
+                        'avatar' : {
+                            'id' : resp['profile']['avatarImgId'],
+                            'url' : resp['profile']['avatarUrl']
+                        },
+                        'background' : {
+                            'id' : resp['profile']['backgroundImgId'],
+                            'url' : resp['profile']['backgroundUrl']
+                        }
                     },
                     'birthday' : resp['profile']['birthday'],
                     'follows' : resp['profile']['followeds'],
                     'followers' : resp['profile']['follows'],
-                    'profileUrl' : f"https://music.163.com/#/user?id={resp['profile']['userId']}" 
+                    'dateCreated' : resp['profile']['createTime'],
+                    'daysCreated' : resp['createDays'],
+                    'isVip' : True if resp['profile']['vipType'] > 0 else False,
+                    'url' : f"https://music.163.com/#/user?id={resp['profile']['userId']}" 
                 }
                 self.userData.update(profile = profile_data)
-
                 # Retrieve playlists infomation
                 self.userData['playlists'].update(count = resp['profile']['playlistCount'])
                 self.userData['playlists'].update(subscribers = resp['profile']['playlistBeSubscribedCount'])
+
+                return self.userData
         except Exception as e:
             print(f'ERROR: {type(e).__name__} - {e}')
 
-    def get_user(self):
+
+    def is_login(self):
+        resp = requests.get(
+            '{}login/status'.format(self.baseUrl),
+            headers = self.headers
+        ).json()
+
         try:
-            # get the current song info
+            if resp['code'] != 200:
+                raise RuntimeError('Invalid! You are not logged in.')
+            else:
+                return resp['profile']
+        except Exception as e:
+            print(f'ERROR: {type(e).__name__} - {e}')
+
+    
+    def get_subcount(self):
+        resp = requests.get(
+            '{}user/subcount'.format(self.baseUrl),
+            headers = self.headers
+        ).json()
+
+        try:
+            if resp['code'] != 200:
+                raise RuntimeError('Invalid! You are not logged in.')
+            else:
+                data = {
+                    'mvCount' : resp['mvCount'],
+                    'djRadioCount' : resp['djRadioCount'],
+                    'createDjRadioCount' : resp['createDjRadioCount'],
+                    'artistCount' : resp['artistCount'],
+                    'createdPlaylistCount' : resp['createdPlaylistCount'],
+                    'subPlaylistCount' : resp['subPlaylistCount']
+                }
+                return data
+        except Exception as e:
+            print(f'ERROR: {type(e).__name__} - {e}')
+
+
+    def logout(self):
+        try:
             resp = requests.get(
-                '{}login/status'.format(self.baseUrl),
+                '{}logout'.format(self.baseUrl),
                 headers = self.headers
             ).json()
 
             if resp['code'] != 200:
                 raise RuntimeError('You are not logged in!')
-            else:
-                self.userData['profile'] = resp['profile']
-
+            print("Successfully logged out!")
+            return resp
         except Exception as e:
             print(f'ERROR: {type(e).__name__} - {e}')
 
 
 if __name__ == "__main__":
     u = User()
-    u.login(email='', pwd='')
-    pprint(u.userData)    
+    print(u.is_login())
+    # u.login(email='@126.com', pwd='')
+    pprint(u.get_subcount())
